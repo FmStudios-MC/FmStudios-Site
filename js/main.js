9111d7d3354f467ae18b6e597f2cc3d4e105ce84
FabiMvurice Interactive <fabiankreissler@outlook.de>
2026-01-22 08:41:19 +0100

    Comment out createParticles call in init function
    Comment out the createParticles function call in init.

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ function renderModpackFilters() {
 
 // Initialize everything
 function init() {
-    createParticles();
+   // createParticles(); //
     renderSocialLinks();
     renderSupportLinks();
     renderFooterSocial();
